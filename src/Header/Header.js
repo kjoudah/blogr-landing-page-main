d38a2db988635102f90a5cc54e688b52374ed3a5
Karam <karamjoudah@hotmail.com>
2023-05-23 21:03:07 -0400

    First version of complete navigation for both mobile and desktop

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import logo from './../images/logo.svg';
+
 export default function Header() {
   return (
     <header className="sticky top-0 left-0 header-pattern-mobile p-4 flex flex-col gap-8 rounded-bl-[5rem]">
@@ -12,38 +13,57 @@ export default function Header() {
 const NavBar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <>
-      <div className="flex flex-row items-center justify-between font-display">
-        {open && (
-          <div
-            onClick={() => setOpen(false)}
-            className="absolute h-screen w-screen inset-0 bg-transparent"
-          ></div>
+    <nav className="flex flex-row items-center justify-between font-display p-4 md:px-16">
+      <a href="#">
+        <img className="w-2/3" src={logo} alt="Blogr Logo"></img>
+      </a>
+
+      <button className="md:hidden" onClick={() => setOpen(!open)}>
+        <span className="sr-only">Open Menu</span>
+        {open ? (
+          <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M23.607.98l1.414 1.413L14.414 13l10.607 10.607-1.414 1.414L13 14.414 2.393 25.021.98 23.607 11.586 13 .98 2.393 2.393.98 13 11.586 23.607.98z"
+              fill="#FFF"
+              fill-rule="evenodd"
+            />
+          </svg>
+        ) : (
+          <svg width="32" height="18" xmlns="http://www.w3.org/2000/svg">
+            <g fill="#FFF" fill-rule="evenodd">
+              <path d="M0 0h32v2H0zM0 8h32v2H0zM0 16h32v2H0z" />
+            </g>
+          </svg>
         )}
-        <a href="#">
-          <img className="w-2/3" src={logo} alt="Blogr Logo"></img>
-        </a>
-        <button className="" onClick={() => setOpen(!open)}>
-          <span className="sr-only">Open Menu</span>
-          {open ? (
-            <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M23.607.98l1.414 1.413L14.414 13l10.607 10.607-1.414 1.414L13 14.414 2.393 25.021.98 23.607 11.586 13 .98 2.393 2.393.98 13 11.586 23.607.98z"
-                fill="#FFF"
-                fill-rule="evenodd"
-              />
-            </svg>
-          ) : (
-            <svg width="32" height="18" xmlns="http://www.w3.org/2000/svg">
-              <g fill="#FFF" fill-rule="evenodd">
-                <path d="M0 0h32v2H0zM0 8h32v2H0zM0 16h32v2H0z" />
-              </g>
-            </svg>
-          )}
-        </button>
-        <MobileMenu open={open} />
+      </button>
+
+      <div className="hidden md:flex flex-row justify-between w-full mx-8 items-center">
+        <ul className="flex flex-row gap-10 items-center">
+          <NavItemDesktop
+            title="Product"
+            items={[
+              'Overview',
+              'Pricing',
+              'Marketplace',
+              'Features',
+              'Integrations',
+            ]}
+          />
+          <NavItemDesktop
+            title="Company"
+            items={['About', 'Team', 'Blog', 'Careers']}
+          />
+          <NavItemDesktop
+            title="Connect"
+            items={['Contact', 'Newsletter', 'LinkedIn']}
+          />
+        </ul>
+
+        <LoginAndSignup />
       </div>
-    </>
+
+      <MobileMenu open={open} />
+    </nav>
   );
 };
 
@@ -90,7 +110,7 @@ const MobileMenu = props => {
   return (
     <div
       data-open={props.open}
-      className="data-[open=false]:hidden data-[open=true]:visible absolute w-11/12 top-[15%] left-0 right-0 mx-auto shadow-xl bg-white rounded-md p-6 "
+      className="data-[open=false]:hidden data-[open=true]:visible absolute w-11/12 top-[15%] left-0 right-0 mx-auto shadow-xl bg-white rounded-md p-6 md:hidden"
     >
       <ul className="flex flex-col justify-center items-center gap-6">
         <NavItemMobile
@@ -128,7 +148,7 @@ const NavItemMobile = props => {
       <button
         onClick={() => setOpen(!open)}
         data-open={open}
-        className="flex flex-row items-center gap-2 text-blue-800 font-display text-lg font-semibold"
+        className="flex flex-row items-center gap-2 text-blue-800 font-display text-lg font-semibold md:text-white md:text-sm"
       >
         {props.title}
         <svg
@@ -136,14 +156,9 @@ const NavItemMobile = props => {
           xmlns="http://www.w3.org/2000/svg"
           width="10"
           height="7"
-          className="data-[open=false]:rotate-0 data-[open=true]:-rotate-180 transition-transform duration-300"
+          className="stroke-red-100 data-[open=false]:rotate-0 data-[open=true]:-rotate-180 transition-transform duration-300 md:stroke-white"
         >
-          <path
-            fill="none"
-            stroke="#FF7B86"
-            stroke-width="2"
-            d="M1 1l4 4 4-4"
-          />
+          <path fill="none" stroke-width="2" d="M1 1l4 4 4-4" />
         </svg>
       </button>
       <div
@@ -164,14 +179,56 @@ const NavItemMobile = props => {
   );
 };
 
+const NavItemDesktop = props => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="relative flex flex-col gap-4 items-center">
+      <button
+        onClick={() => setOpen(!open)}
+        data-open={open}
+        className="flex flex-row items-center gap-2 text-blue-800 font-display text-lg font-semibold md:text-white md:text-sm"
+      >
+        {props.title}
+        <svg
+          data-open={open}
+          xmlns="http://www.w3.org/2000/svg"
+          width="10"
+          height="7"
+          className="stroke-red-100 data-[open=false]:rotate-0 data-[open=true]:-rotate-180 transition-transform duration-300 md:stroke-white"
+        >
+          <path fill="none" stroke-width="2" d="M1 1l4 4 4-4" />
+        </svg>
+      </button>
+      <div
+        data-open={open}
+        className="absolute bg-white p-4 top-[130%] w-32 data-[open=false]:hidden data-[open=true]:visible rounded-md py-4"
+      >
+        <ul className="flex flex-col justify-center items-start gap-4">
+          {props.items.map(item => {
+            return (
+              <li className="text-blue-800 font-body font-medium text-sm hover:font-semibold">
+                <a href="#">{item}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </li>
+  );
+};
+
 const LoginAndSignup = () => {
   return (
-    <div className="flex flex-col gap-2 justify-center items-center">
-      <a className="font-heading text-blue-800 text-lg font-semibold" href="#">
+    <div className="flex flex-col gap-2 justify-center items-center md:flex-row md:gap-4">
+      <a
+        className="font-heading text-blue-800 text-lg font-semibold md:text-white md:text-sm"
+        href="#"
+      >
         Login
       </a>
       <a
-        className="font-body font-medium w-28 text-center text-white px-4 py-2 bg-gradient-to-r from-red-100 to-red-200 rounded-full"
+        className="font-body font-medium w-28 font-medium text-center text-white px-4 py-2 bg-gradient-to-r from-red-100 to-red-200 rounded-full md:bg-none md:bg-white md:text-red-100 md:text-sm"
         href="#"
       >
         Sign Up
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: [
+    './src/**/*.{js,jsx}',
+    './node_modules/flowbite/**/*.js',
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     fontFamily: {
       heading: 'Overpass,sans-serif',
@@ -28,5 +32,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
